de89dec03bc018d8a3e456ef2a5d80c9a630751e
Eric Moy <ericzmoy@gmail.com>
2023-08-09 23:28:11 -0500

    Create play_again function sequence

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,41 @@ use rand::Rng;
 
 use colored::Colorize;
 
+
 fn main() {
+    game();
+    play_again_sequence();
+
+}
+
+fn play_again_sequence() {
+    println!("Do you want to play again? (y/n)");
+                let mut play_again: String = String::new();
+
+                play_again.clear();
+                io::stdin()
+                    .read_line(&mut play_again)
+                    .expect("Failed to read line");
+
+                    
+                    match play_again.trim() {
+                        "n" | "N" => {
+                            println!("Exiting game");
+                        },
+                        &_ => {
+                            println!("Restarting game...");
+                            main()
+                        }
+                    }
+}
+
+fn game() {
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    // Uncomment to see secret number
+    // println!("{}", &secret_number.to_string());
 
     loop {
         println!("Please input your number...");
@@ -30,7 +61,7 @@ fn main() {
             Ordering::Greater => println!("{}", "Too big".red()),
             Ordering::Equal => {
                 println!("{}", "You win!".green());
-                break;
+                break();
             },
         }
     }
